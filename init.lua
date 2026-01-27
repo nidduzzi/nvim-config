b526ae03c43ba34e4a17dececba1bdb30d51862b
Ahmad <ahmad@mediainfogrup.com>
2026-01-27 21:29:43 +0700

    fix lsp config to use user configs

--- init.lua
+++ init.lua
@@ -549,21 +549,76 @@ require('lazy').setup({
       -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
     },
-    opts = {
-      servers = {
-        pylsp = {
-          -- pylsp = {
-          plugins = {
-            ruff = {
-              enabled = true, -- Enable the plugin
-              formatEnabled = true, -- Enable formatting using ruffs formatter
+    opts_extend = { 'servers.*.keys' },
+    opts = function()
+      ---@class PluginLspOpts
+      local ret = {
+        servers = {
+          pylsp = {
+            capabilities = {
+              textDocument = {
+                completion = false,
+              },
+            },
+            settings = {
+              plugins = {
+                ruff = {
+                  enabled = true, -- Enable the plugin
+                  formatEnabled = true, -- Enable formatting using ruffs formatter
+                },
+                jedi_completion = { enabled = false },
+                jedi_definition = { enabled = false },
+                jedi_references = { enabled = false },
+                jedi_hover = { enabled = false },
+                jedi_signature_help = { enabled = false },
+                jedi_symbols = { enabled = false },
+                jedi_type_definition = { enabled = false },
+                rope_completion = { enabled = false },
+              },
             },
           },
-          -- },
+          pyrefly = {
+            on_exit = function(code, _, _)
+              vim.schedule(function()
+                vim.notify('Closing Pyrefly LSP exited with code: ' .. code, vim.log.levels.INFO)
+              end)
+            end,
+          },
+
+          -- rust_analyzer = {},
+          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+          --
+          -- Some languages (like typescript) have entire language plugins that can be useful:
+          --    https://github.com/pmizio/typescript-tools.nvim
+          --
+          -- But for many setups, the LSP (`ts_ls`) will work just fine
+          --
+          -- ts_ls = {},
+          -- biome = { formatEnabled = true },
+
+          lua_ls = {
+            -- cmd = { 'test' },
+            -- filetypes = { ... },
+            -- capabilities = {},
+            settings = {
+              Lua = {
+                workspace = {
+                  library = { vim.env.VIMRUNTIME },
+                },
+                completion = {
+                  callSnippet = 'Replace',
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                -- diagnostics = { disable = { 'missing-fields' } },
+              },
+            },
+          },
         },
-      },
-    },
-    config = function()
+      }
+      return ret
+    end,
+    ---@param opts PluginLspOpts
+    config = vim.schedule_wrap(function(_, opts)
       -- Brief aside: **What is LSP?**
       --
       -- LSP is an initialism you've probably heard, but might not understand what it is.
@@ -699,10 +754,11 @@ require('lazy').setup({
 
       -- Diagnostic Config
       -- See :help vim.diagnostic.Opts
+      ---@type vim.diagnostic.Opts
       vim.diagnostic.config {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.ERROR },
+        underline = { severity = { vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN } }, ---@type vim.diagnostic.Opts.Underline
         signs = vim.g.have_nerd_font and {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',
@@ -724,6 +780,7 @@ require('lazy').setup({
             return diagnostic_message[diagnostic.severity]
           end,
         },
+        virtual_lines = false,
       }
 
       -- LSP servers and clients are able to communicate to each other what features they support.
@@ -741,50 +798,16 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- ruff = {},
-        -- NOTE: Install ruff, mypy, and rope with PylspInstall
-        -- pylsp = {
-        --   pylsp = {
-        --     plugins = {
-        --       ruff = {
-        --         enabled = true, -- Enable the plugin
-        --         formatEnabled = true, -- Enable formatting using ruffs formatter
-        --       },
-        --     },
-        --   },
-        -- },
-        -- pylyzer = {},
+      local servers = opts.servers
+      local function configure(server)
+        local sopts = opts.servers[server]
+        sopts = sopts == true and {} or (not sopts) and { enabled = false } or sopts --[[@as lazyvim.lsp.Config]]
+        vim.lsp.config(server, sopts) -- configure the server
+        vim.lsp.enable(server)
+        return true
+      end
 
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        --
-        -- ts_ls = {},
-        -- biome = { formatEnabled = true },
-
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        },
-      }
+      vim.tbl_filter(configure, vim.tbl_keys(opts.servers))
 
       -- Ensure the servers and tools above are installed
       --
@@ -799,10 +822,10 @@ require('lazy').setup({
       --
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
+      local ensure_installed = {
+        'lua_ls',
         'stylua', -- Used to format Lua code
-      })
+      }
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
@@ -815,11 +838,11 @@ require('lazy').setup({
             -- by the server configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            vim.lsp.config(server_name, server)
           end,
         },
       }
-    end,
+    end),
   },
 
   { -- Autoformat
@@ -954,7 +977,7 @@ require('lazy').setup({
       sources = {
         default = { 'lsp', 'path', 'snippets', 'lazydev' },
         providers = {
-          lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
         },
       },
 
